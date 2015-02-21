8abd8387f2feb5f1149dc6c4724ae9a34b498d68
CristianoAzevedo <cristianodasilva.azevedo@gmail.com>
2015-02-21 16:06:19 -0200

    Alterações

--- src/main/webapp/js/IndexController.js
+++ src/main/webapp/js/IndexController.js
@@ -1,9 +1,25 @@
 	//Definindo o módulo	
-	modulo = angular.modulo("App",[]);
+	modulo = angular.module("App",[]);
 	
-	modulo.controller("IndexController", function($scope){
+	modulo.service('serviceIndex', ['$http', function($http){
+		this.setAluno = function (aluno){
+			return $http.post("/aluno?nome="+aluno.nome+'&ra='+aluno.ra);
+		}
+	}]);
+	
+	modulo.controller("IndexController", ['$scope', 'serviceIndex', function($scope, serviceIndex){
 		
-	});
+		$scope.alunos = [];
+		
+		$scope.cadastrar = function(aluno){
+			serviceIndex.setAluno(aluno).success(function(data){
+				$scope.alunos.push(data);				
+			}).error(function(data){
+				console.log('Não funfou!');
+			})
+		}
+		
+	}]);
 
 
 	
