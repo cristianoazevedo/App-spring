3bf647fee2f75eab443d582a265824b227314acf
CristianoAzevedo <cristianodasilva.azevedo@gmail.com>
2015-02-24 23:58:25 -0300

    Alteração de métodos

--- src/main/java/demo/IndexController.java
+++ src/main/java/demo/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@RequestMapping(value="/aluno")
-	public Aluno aluno(@RequestParam(value="nome") String nome, @RequestParam(value="ra") String ra){
+	public Aluno requestAluno(@RequestParam(value="nome") String nome, @RequestParam(value="ra") String ra){
 		return new Aluno(nome, ra);
 	}
 }
